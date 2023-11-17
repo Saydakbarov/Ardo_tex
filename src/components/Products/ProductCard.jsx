d9337e35cabb922b9d56aef58380d78046dfb346
Akhgofur <gofurbekaxmadjonov397@gmail.com>
2023-11-17 16:40:29 +0300

    check

--- src/components/Products/ProductCard.jsx
+++ src/components/Products/ProductCard.jsx
@@ -38,6 +38,7 @@ export default function ProductCard({ data }) {
             style={{
               width: "100%",
               height: "300px",
+              objectFit: "contain"
             }}
             src={v?.product_image_url[0]}
             alt=""
