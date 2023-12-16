e185f8cb209ff281ea3740c3abb6ffbc719984d1
Akhgofur <gofurbekaxmadjonov397@gmail.com>
2023-12-16 14:54:19 +0300

    done

--- src/components/Home/Banner.jsx
+++ src/components/Home/Banner.jsx
@@ -1,44 +1,8 @@
-import { Box, Button, Typography } from "@mui/material"
-import { useTranslation } from "react-i18next"
-
-export const Banner = () => {
-
-    const {t} = useTranslation()
+export const Banner = ({src}) => {
 
     return (
-        <div className="mx-auto px-3 max-w-[1500px] w-full">
-            <Box
-        sx={{
-          backgroundImage:
-            "url('https://wpbingosite.com/wordpress/furety/wp-content/uploads/2023/05/back.jpg')",
-          minHeight: "400px",
-          backgroundPosition: "100%",
-          mt: 10,
-          position: "relative",
-        }}
-      >
-        <Box
-          sx={{
-            position: "absolute",
-            width: { xs: "250px", sm: "300px", md: "350px", lg: "400px" },
-            top: "30%",
-            left: "5%",
-          }}
-        >
-          <Typography sx={{ fontSize: "34px" }}>
-            Pebble Ottoman
-          </Typography>
-          <Typography
-            sx={{ fontSize: "16px",  mt: 2, color: "gray" }}
-          >
-            Kiln-dried engineered wood frame with reinforced joinery. webbed
-            seat support
-          </Typography>
-          <Button sx={{ mt: 3 }} variant="outlined">
-            {t("shop-now")}
-          </Button>
-        </Box>
-      </Box>
-        </div>
+       <div className="py-[30px]">
+        <img src={src} alt="" className="w-full h-auto" />
+       </div>
     )
 }
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -19,7 +19,7 @@ export default function HomePage() {
       <HeaderBox />
       <NewCollections />
       <BestSeller />
-      <Banner />
+      <Banner src={"/img/banner.png"} />
       <DiscountedProducts />
       <LastestNews />
       <HomeCategory />
