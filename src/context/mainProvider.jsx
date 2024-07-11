bbe3602498a49a37aa59daf3934497ccfa905125
Akhgofur <gofurbekaxmadjonov397@gmail.com>
2024-07-11 11:13:02 +0500

    update

--- src/context/mainProvider.jsx
+++ src/context/mainProvider.jsx
@@ -4,12 +4,12 @@ import { useSlug } from "../query-data/data.service"
 
 export const MainProvider = ({children}) => {
 
-    const {data, refetch} = useSlug()
+    // const {data, refetch} = useSlug()
 
-    useEffect(() => {
-        refetch()
-    }, [window.location.pathname])
+    // useEffect(() => {
+    //     refetch()
+    // }, [window.location.pathname])
 
-    return data?.data?.value ? <p className="text-center py-[50px] text-3xl font-semibold">{"Page not found("}</p> : children
+    return  children
  
 }
